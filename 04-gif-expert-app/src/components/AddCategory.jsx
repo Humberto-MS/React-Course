36754901cb0b0f02794dbc61cdb492b60d20b6f7
Humberto Medina <humberto.ms123@gmail.com>
2025-02-03 22:53:56 -0600

    Finish App 04

--- 04-gif-expert-app/src/components/AddCategory.jsx
+++ 04-gif-expert-app/src/components/AddCategory.jsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import { useState } from "react"
 
 export const AddCategory = ( { onNewCategory } ) => {
@@ -12,7 +13,7 @@ export const AddCategory = ( { onNewCategory } ) => {
     };
     
     return (
-        <form onSubmit={onSubmit}>
+        <form onSubmit={onSubmit} aria-label="form">
             <input 
                 type="text"
                 placeholder="Buscar Gifs"
@@ -20,4 +21,8 @@ export const AddCategory = ( { onNewCategory } ) => {
                 onChange={onInputChange}/>
         </form>
     )
+}
+
+AddCategory.propTypes = {
+    onNewCategory: PropTypes.func.isRequired
 }
--- 04-gif-expert-app/src/helpers/getGifs.js
+++ 04-gif-expert-app/src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async ( category ) => {
-    const api_key = 'ydW5Cz4508kwodvInIF2HPlvcB6R2rqK';
+    const api_key = '';
     const url = `https://api.giphy.com/v1/gifs/search?api_key=${ api_key }&q=${ category }&limit=20`;
     const resp = await fetch ( url );
     const { data } = await resp.json();
